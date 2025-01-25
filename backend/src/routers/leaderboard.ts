e01b2e508d1d709eba317bb12401a843817b0bbb
Cameron <112224450+thecamerondev@users.noreply.github.com>
2025-01-25 10:46:56 -0500

    patch(backend): Leaderboard

--- backend/src/routers/leaderboard.ts
+++ backend/src/routers/leaderboard.ts
@@ -4,6 +4,10 @@ import { PrismaClient, User, Stock } from '@prisma/client';
 const leadRouter = Router();
 const prisma = new PrismaClient();
 
+interface Holdings {
+  data: Holding[];
+}
+
 interface Holding { //To be used in representing the User.holding objects
   stock: string; // Stock symbol
   amount: number; // Quantity of stock held
@@ -14,11 +18,9 @@ leadRouter.get('/', async (req, res) => {
     const users = await prisma.user.findMany();
     const stocks = await prisma.stock.findMany();
     const leaderboard = users.map(user => {
-      const holdings: Holding[] = JSON.parse(
-        JSON.stringify(user.holding || [])
-      ) as Holding[];
+      const holdings = (user.holding as any)?.data || [];
       
-      const totalBalance = holdings.reduce((total, holding) => {
+      const totalBalance = holdings.reduce((total: number, holding: Holding) => {
         const stock = stocks.find(s => s.symbol === holding.stock);
         if (stock) {
           total += stock.price[stock.price.length - 1] * holding.amount;
