693f9a94cfff52506d94ff4ac4b0c4a821e98a70
millerding <mding022@uottawa.ca>
2025-01-24 21:59:56 -0500

    First commit

---
+++ src/test/java/com/fakenewscorp/bullorbust/BullOrBustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakenewscorp.bullorbust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BullOrBustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
