51750675d27c38ab34372076742564caa5fe6f66
Cameron <112224450+thecamerondev@users.noreply.github.com>
2025-01-25 01:57:24 -0500

    feat: Websocket support

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,30 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import expressWs from 'express-ws';
 
 dotenv.config();
 
-const app: Express = express();
+const { app, getWss, applyTo } = expressWs(express());
 const port = process.env.PORT || 3000;
+
+const router = express.Router() as expressWs.Router;
+
+router.ws('/echo', (ws, req) => {
+  ws.on('message', (msg: String) => {
+      ws.send(msg);
+  });
+
+  console.log('Connection opened');
+  ws.send('Hello World!');
+
+  setInterval(() => {
+    ws.send('Hello World!');
+  }, 1000);
+
+  ws.on('close', () => {
+    console.log('Connection closed');
+  });
+});
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).send("OK");
@@ -13,3 +33,5 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
+
+app.use('/ws', router);
